73ee5254a563e4745197fafbaf324fce47504d94
Syed Hoque <nadeem.redwanul@hotmail.com>
2022-09-02 13:49:13 +0100

    Initial commit

---
+++ AppiumSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppiumSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppiumSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppiumSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
